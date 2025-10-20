76326b22c6b70a4851df6ba8aa297dc0aebbb619
Jackson Williams <jacksonwilliams.williams@gmail.com>
2025-10-20 16:56:23 -0600

    finishes up landing page, sets up routing to quiz

--- victimology-quiz-site/src/app/app.routes.ts
+++ victimology-quiz-site/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {  LandingComponent } from './landing/landing.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'results', component: ResultsComponent },
 ];
